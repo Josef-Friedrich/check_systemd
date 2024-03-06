f8159047276bd1a4a1da1f9af361aec7c9eb7993
Josef Friedrich <josef@friedrich.rocks>
2024-03-06 22:18:30 +0100

    Test gi

---
+++ tests/_test_gi.py
@@ -0,0 +1,23 @@
+"""Run this test in an unmocked environment."""
+
+from __future__ import annotations
+
+import pytest
+
+
+from gi.repository.Gio import BusType, DBusProxy, DBusProxyFlags
+
+proxy = DBusProxy.new_for_bus_sync(
+    BusType.SESSION,
+    DBusProxyFlags.DO_NOT_LOAD_PROPERTIES,
+    None,
+    "org.freedesktop.systemd1",
+    "/org/freedesktop/systemd1/unit/update_2dmotd_2etimer",
+    "org.freedesktop.DBus.Properties",
+    None,
+)
+
+
+print(proxy.GetAll("(ss)", "org.freedesktop.systemd1.Timer", "NextElapseUSecMonotonic"))
+
+print(proxy.Get("(ss)", "org.freedesktop.systemd1.Timer", "NextElapseUSecMonotonic"))
