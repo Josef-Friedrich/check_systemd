5d72ab232d2682d6932a78bdd4469793fd3dc1c2
Josef Friedrich <josef@friedrich.rocks>
2024-04-12 17:45:17 +0200

    Update lock file

--- tests/_test_gi.py
+++ tests/_test_gi.py
@@ -2,19 +2,22 @@
 
 from __future__ import annotations
 
+from time import sleep
+
 from gi.repository.Gio import BusType, DBusProxy, DBusProxyFlags
 
-proxy = DBusProxy.new_for_bus_sync(
-    BusType.SESSION,
-    DBusProxyFlags.DO_NOT_LOAD_PROPERTIES,
-    None,
-    "org.freedesktop.systemd1",
-    "/org/freedesktop/systemd1/unit/update_2dmotd_2etimer",
-    "org.freedesktop.DBus.Properties",
-    None,
-)
+# print(proxy.GetAll("(ss)", "org.freedesktop.systemd1.Timer", "NextElapseUSecMonotonic"))  # type: ignore
 
 
-print(proxy.GetAll("(ss)", "org.freedesktop.systemd1.Timer", "NextElapseUSecMonotonic"))  # type: ignore
-
-print(proxy.Get("(ss)", "org.freedesktop.systemd1.Timer", "NextElapseUSecMonotonic"))  # type: ignore
+while True:
+    proxy = DBusProxy.new_for_bus_sync(
+        BusType.SESSION,
+        DBusProxyFlags.DO_NOT_LOAD_PROPERTIES,
+        None,
+        "org.freedesktop.systemd1",
+        "/org/freedesktop/systemd1/unit/example_2dtimer_2etimer",
+        "org.freedesktop.DBus.Properties",
+        None,
+    )
+    print(proxy.Get("(ss)", "org.freedesktop.systemd1.Timer", "NextElapseUSecRealtime"))  # type: ignore
+    sleep(10)
